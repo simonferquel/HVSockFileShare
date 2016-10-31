36a8b67f4b5870db3b9a331c33724748435d3896
Simon Ferquel <simon.ferquel@docker.com>
2016-10-31 17:06:13 +0100

    Very fast connection check
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- src/Transport/Connect_Linux.cpp
+++ src/Transport/Connect_Linux.cpp
@@ -7,8 +7,24 @@ SafeSocket HVFiles::Connect(const GUID & partition, const GUID & service) {
     addr.Reserved = 0;
     addr.VmId = partition;
     addr.ServiceId = service;
+	int flags = fcntl(s.get(), F_GETFL, 0);
+	fcntl(fd, F_SETFL, flags | O_NONBLOCK);
     if (0 != connect(s.get(), reinterpret_cast<const sockaddr*>(&addr), sizeof(addr))) {
-        throw ConnectionFailedException(errno);
+		auto err = errno;
+		if (err != EWOULDBLOCK) {
+			throw ConnectionFailedException(err);
+		}
+		fd_set fds;
+		FD_ZERO(&fds);
+		FD_SET(s.get(), &fds);
+		timeval timeout;
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 1000 * 30; // 30ms is long enough for hv_socks
+		if (select(1, nullptr, &fds, nullptr, &timeout) != 1) {
+			// timeout 
+			throw ConnectionFailedException(err);
+		}
     }
+	fcntl(fd, F_SETFL, flags);
     return s;
 }
